066afa96b8d3b3693c2b66687a7c256c74639816
Abdul Aziz <otakcerdascom@gmail.com>
2019-01-16 16:07:20 +0700

    halaman login baru

--- application/views/login_view.php
+++ application/views/login_view.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="en">
+<!-- <html lang="en">
   <head>
     <meta charset="utf-8">
     <title>Sign In</title>
@@ -24,8 +24,45 @@
            <button class="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
          </form>
        </div>
-       </div> <!-- /container -->
+       </div> 
 
     <script src="<?php echo base_url('assets/js/bootstrap.min.js');?>"></script>
+  </body>
+</html> -->
+
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="../../../../favicon.ico">
+
+    <title>Signin Sistem Absensi Cuti Pegawai</title>
+
+ 
+    <link href="<?php echo base_url('assets/css/bootstrap.min.css');?>" rel="stylesheet">
+
+
+    <link href="<?php echo base_url('assets/css/signin.css"');?> rel="stylesheet">
+  </head>
+
+  <body class="text-center">
+    <form class="form-signin" action="<?php echo site_url('login/auth');?>" method="post">
+      <img class="mb-4" src="<?php echo base_url('assets/image/login.png');?>" alt="" width="72" height="60">
+      <h1 class="h3 mb-3 font-weight-normal">Please sign in</h1>
+      <?php echo $this->session->flashdata('msg');?>
+      <label for="inputEmail" class="sr-only">Email address</label>
+      <input type="email" name="email" id="inputEmail" class="form-control" placeholder="Email address" required autofocus>
+      <label for="inputPassword" class="sr-only">Password</label>
+      <input type="password" name="password" id="inputPassword" class="form-control" placeholder="Password" required>
+      <div class="checkbox mb-3">
+        <label>
+          <input type="checkbox" value="remember-me"> Remember me
+        </label>
+      </div>
+      <button class="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
+      <p class="mt-5 mb-3 text-muted">&copy; 2019</p>
+    </form>
   </body>
 </html>
